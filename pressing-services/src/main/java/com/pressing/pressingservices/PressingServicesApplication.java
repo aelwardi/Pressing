f4cefbd1ce5a7e6e822e280c1c17374101522ecc
aelwardi <a.elwardi98@gmail.com>
2024-02-09 20:30:03 +0100

    Get Starting ....

---
+++ pressing-services/src/main/java/com/pressing/pressingservices/PressingServicesApplication.java
@@ -0,0 +1,13 @@
+package com.pressing.pressingservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PressingServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PressingServicesApplication.class, args);
+	}
+
+}
---
+++ pressing-services/src/test/java/com/pressing/pressingservices/PressingServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pressing.pressingservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PressingServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
